7c176722beaef716c39616a438967b407d32c256
wangximing <wangximing@cryptape.com>
2021-09-03 10:24:45 +0800

    feat: add go back button on header

--- packages/app-ui-issuer/src/components/header.tsx
+++ packages/app-ui-issuer/src/components/header.tsx
@@ -1,21 +1,30 @@
+import { LeftOutlined } from '@ant-design/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
 interface IProps {
   title?: string;
 }
 const HeaderWrapper = styled.div`
-  .header {
-    height: 40px;
-    line-height: 40px;
-    text-align: center;
-    background: rgba(0, 0, 0, 0.8);
-    color: white;
-  }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
+  padding: 0 10px;
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
 `;
 const Header: React.FC<IProps> = (props: IProps) => {
+  const history = useHistory();
+  const goBack = () => {
+    history.go(-1);
+  };
   return (
     <HeaderWrapper>
+      <LeftOutlined onClick={goBack} />
       <div className="header">{props.title}</div>
+      <div></div>
     </HeaderWrapper>
   );
 };
