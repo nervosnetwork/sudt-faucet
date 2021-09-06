d8d81b40f2e13841464d834a1f34dba32b9ddb7b
wangximing <wangximing@cryptape.com>
2021-09-06 09:13:15 +0800

    feat: add header text for each component

--- packages/app-ui-issuer/src/pages/issuer.tsx
+++ packages/app-ui-issuer/src/pages/issuer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from '../components/header';
+import { routeList } from '../router';
 import CreateToken from './createToken';
 import IssueToken from './issueToken';
 import Login from './login';
@@ -8,36 +9,57 @@ import TokenCharge from './tokenCharge';
 import TokenDetail from './tokenDetail';
 import TokenList from './tokenList';
 import TokenManagement from './tokenManagement';
+import { WapperComponent } from './wapperComponent';
 
 export const Issuer: React.FC = () => {
   return (
     <div className="app">
       <Router>
-        <Header />
         <Switch>
+          {/* {routeList.map((item) => (
+            <Route path={item.path} key={item.path}>
+              <WapperComponent title={item.title}>{item.component}</WapperComponent>
+            </Route>
+          ))} */}
           <Route path="/login">
-            <Login />
+            <WapperComponent title="Token Issuer">
+              <Login />
+            </WapperComponent>
           </Route>
           <Route path="/token-list">
-            <TokenList />
+            <WapperComponent title="Token Issuer">
+              <TokenList />
+            </WapperComponent>
           </Route>
           <Route path="/create-token">
-            <CreateToken />
+            <WapperComponent title="Token Issuer">
+              <CreateToken />
+            </WapperComponent>
           </Route>
           <Route path="/token-detail">
-            <TokenDetail />
+            <WapperComponent title="Token Issuer">
+              <TokenDetail />
+            </WapperComponent>
           </Route>
           <Route path="/issue-token">
-            <IssueToken />
+            <WapperComponent title="Token Issuer">
+              <IssueToken />
+            </WapperComponent>
           </Route>
           <Route path="/token-management">
-            <TokenManagement />
+            <WapperComponent title="Token Issuer">
+              <TokenManagement />
+            </WapperComponent>
           </Route>
           <Route path="/token-charge">
-            <TokenCharge />
+            <WapperComponent title="Token Issuer">
+              <TokenCharge />
+            </WapperComponent>
           </Route>
           <Route path="/">
-            <Login />
+            <WapperComponent title="Token Issuer">
+              <Login />
+            </WapperComponent>
           </Route>
         </Switch>
       </Router>
