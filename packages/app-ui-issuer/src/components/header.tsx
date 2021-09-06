cd735887ac1d098b4a58c34e271858202d200cef
homura <homura.dev@gmail.com>
2021-09-06 22:54:49 +0800

    feat: work with rc lock

--- packages/app-ui-issuer/src/components/header.tsx
+++ packages/app-ui-issuer/src/components/header.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import { WalletContainer } from '../containers';
+import { Address } from './address';
+
 interface IProps {
   title?: string;
 }
+
 const HeaderWrapper = styled.div`
-  .header {
-    height: 40px;
-    line-height: 40px;
-    text-align: center;
-    background: rgba(0, 0, 0, 0.8);
-    color: white;
-  }
+  height: 40px;
+  line-height: 40px;
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
+  padding: 0 24px;
+  display: flex;
+  justify-content: space-between;
 `;
+
 const Header: React.FC<IProps> = (props: IProps) => {
+  const wallet = WalletContainer.useContainer();
+
   return (
     <HeaderWrapper>
-      <div className="header">{props.title}</div>
+      <div>{props.title}</div>
+      {wallet.stage === 'readyToSign' && (
+        <div>
+          <Address address={wallet.address} />
+        </div>
+      )}
     </HeaderWrapper>
   );
 };
