2454432eb119fbec64422ff594923dcda67e08d0
mkxbl <33241113+mkxbl@users.noreply.github.com>
2021-09-08 13:43:20 +0800

    refactor: optimize sendgrid (#13)

--- packages/app-server-issuer/src/db/setup_mysql.sh
+++ packages/app-server-issuer/src/db/setup_mysql.sh
@@ -1,5 +1,8 @@
+set -uxe
+
 docker run --rm -p 3306:3306 --name sudt-faucet-mysql -d -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
 sleep 15
 docker exec sudt-faucet-mysql mysql -uroot -p123456 --protocol=tcp -e "create database sudt_faucet"
 
-knex migrate:latest --knexfile ./knexfile.ts --env development
+DB_DIR="$(dirname "${BASH_SOURCE[0]}")"
+knex migrate:latest --knexfile "${DB_DIR}"/knexfile.ts --env development
