fb42b588424c71e93f8cd173475273b085f7b4db
方平 <fpchen@outlook.com>
2021-09-09 21:08:01 +0800

    feat:  deploy by docker (#20)
    * fix: update deploy param
    
    * fix: update deploy param
    
    * fix: replace host with env
    
    * fix: update big int
    
    * feat: add docker
    
    * feat: add docker
    
    * feat: remove mysql
    
    * fix: update big number
    
    * fix: update docker

--- packages/app-server-issuer/src/db/knexfile.ts
+++ packages/app-server-issuer/src/db/knexfile.ts
@@ -1,10 +1,13 @@
 const password = process.env.MYSQL_PASSWORD || '123456';
+const host = process.env.MYSQL_HOST || '127.0.0.1';
+const port = process.env.MYSQL_PORT || '3306';
 const knexConfig = {
   development: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
+      host: host,
       database: 'sudt_faucet',
+      port: port,
       user: 'root',
       password: password,
     },
