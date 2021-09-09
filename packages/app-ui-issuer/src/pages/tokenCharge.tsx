496b9ba797647d71f4250d43fea17f0f51c21bb3
wangximing <wangximing@cryptape.com>
2021-09-09 15:54:31 +0800

    feat: add token charge validation

--- packages/app-ui-issuer/src/pages/tokenCharge.tsx
+++ packages/app-ui-issuer/src/pages/tokenCharge.tsx
@@ -2,9 +2,11 @@ import { MintRcUdtBuilder } from '@ckitjs/ckit';
 import { Form, Input, Button, Typography, message } from 'antd';
 import { useFormik } from 'formik';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import client from '../configs/client';
+import { WalletContainer } from '../containers';
 import { useListRcSupplyLockUdtQuery, useProvider, useRcSigner, useSendTransaction } from '../hooks';
 
 const StyleWrapper = styled.div`
@@ -21,7 +23,10 @@ interface FormError {
   amount?: string;
 }
 
+const CKB_DECIMAL = 8;
+
 const TokenCharge: React.FC = () => {
+  const wallet = WalletContainer.useContainer();
   const history = useHistory();
   const provider = useProvider();
   const { udtId } = useParams<{ udtId: string }>();
@@ -31,29 +36,44 @@ const TokenCharge: React.FC = () => {
 
   const [chargeAddress, setChargeAddress] = useState<string>();
 
-  const [ckbAmount, setCkbAmount] = useState('0');
-  const [sudtAmount, setSudtAmount] = useState('0');
-
   useEffect(() => {
     void client.get_claimable_account_address().then(setChargeAddress);
   }, []);
+
+  const address = wallet.stage === 'readyToSign' ? wallet.address : '';
+
+  const query = useQuery(
+    ['queryBalance', { address }],
+    () => {
+      return provider.getCkbLiveCellsBalance(address);
+    },
+    {
+      enabled: !!address,
+    },
+  );
+
   const foundUdtInfo = udts?.[0];
-  const validate = (values: FormValues) => {
+  const validate = async (values: FormValues) => {
     if (!foundUdtInfo) return;
+    if (!query.data) return;
     const errors: FormError = {};
     if (!values.capacity) {
       errors.capacity = 'Capacity is Required';
+    } else if (fixedStringToBigint(values.capacity, CKB_DECIMAL) > BigInt(query.data)) {
+      errors.capacity = `Must be less than or equal to ${bigintToFixedString(BigInt(query.data), CKB_DECIMAL)}`;
     }
-    const leftAmount = BigInt(foundUdtInfo.maxSupply) - BigInt(foundUdtInfo.currentSupply);
+    const leftAmount =
+      fixedStringToBigint(foundUdtInfo.maxSupply, foundUdtInfo.decimals) -
+      fixedStringToBigint(foundUdtInfo.currentSupply, foundUdtInfo.decimals);
     if (!values.amount) {
-      errors.amount = 'Capacity is Required';
-    } else if (BigInt(values.amount) >= leftAmount) {
-      errors.amount = `Must be less than or equal to ${leftAmount}`;
+      errors.amount = 'Amount(Ins) is Required';
+    } else if (fixedStringToBigint(values.amount, foundUdtInfo.decimals) > leftAmount) {
+      errors.amount = `Must be less than or equal to ${bigintToFixedString(leftAmount, foundUdtInfo.decimals)}`;
     }
     return errors;
   };
 
-  function charge() {
+  function charge(values: FormValues) {
     if (!chargeAddress) {
       return Promise.reject('Charge address is not loaded');
     }
@@ -64,10 +84,10 @@ const TokenCharge: React.FC = () => {
         recipients: [
           {
             recipient: chargeAddress,
-            amount: sudtAmount,
+            amount: values.amount,
             //TODO findOrCreate
             capacityPolicy: 'createCell',
-            additionalCapacity: ckbAmount,
+            additionalCapacity: values.capacity,
           },
         ],
       },
@@ -87,7 +107,7 @@ const TokenCharge: React.FC = () => {
     validate,
     onSubmit: (values: FormValues) => {
       history.push('/token-list');
-      charge()
+      charge(values)
         .then(() => {
           history.push('/token-list');
         })
@@ -96,38 +116,70 @@ const TokenCharge: React.FC = () => {
         });
     },
   });
+  const fixString = (input: string, decimal: number): string => {
+    const inputArray = input.split('.');
+    if (inputArray.length === 1) return inputArray[0];
+    return `${inputArray[0]}.${inputArray[1].slice(0, decimal)}`;
+  };
+
+  const fixedStringToBigint = (input: string, decimal: number): bigint => {
+    const decimalString = Array(decimal).fill('0').join('');
+    const inputArray = input.split('.');
+    inputArray[1] = inputArray[1] ? `${inputArray[1]}${decimalString}`.slice(0, decimal) : decimalString;
+    return BigInt(inputArray.join(''));
+  };
+
+  const bigintToFixedString = (input: bigint, decimal: number): string => {
+    const inputString = input.toString();
+    return inputString.slice(0, 0 - decimal) + '.' + inputString.slice(0 - decimal);
+  };
 
   if (!chargeAddress) return null;
   return (
     <StyleWrapper>
-      <Form name="basic">
-        <Form.Item label="to">
-          <div>{chargeAddress}</div>
-        </Form.Item>
-        <Form.Item label="Capacity(CKB)" name="capacity">
-          <Input
-            {...formik.getFieldProps('capacity')}
-            value={ckbAmount}
-            onChange={(e) => setCkbAmount(e.target.value)}
-          />
-          <Typography.Text type="danger">{formik.errors.capacity}</Typography.Text>
-        </Form.Item>
+      {foundUdtInfo && (
+        <Form name="basic">
+          <Form.Item label="to">
+            <div>{chargeAddress}</div>
+          </Form.Item>
+          <Form.Item label="Capacity(CKB)" name="capacity">
+            <Input
+              onChange={(e) => {
+                const value = e.target.value.replace(/^\D*(\d*(?:\.\d*)?).*$/g, '$1');
+                void formik.setFieldValue('capacity', fixString(value, CKB_DECIMAL), true);
+              }}
+              value={formik.values.capacity}
+              onBlur={formik.handleBlur}
+            />
+            <Typography.Text type="danger">{formik.errors.capacity}</Typography.Text>
+          </Form.Item>
 
-        <Form.Item label="Amount(Ins)" name="amount">
-          <Input
-            {...formik.getFieldProps('amount')}
-            value={sudtAmount}
-            onChange={(e) => setSudtAmount(e.target.value)}
-          />
-          <Typography.Text type="danger">{formik.errors.amount}</Typography.Text>
-        </Form.Item>
+          <Form.Item label="Amount(Ins)" name="amount">
+            <Input
+              onChange={(e) => {
+                //TODO replace with new RegExp
+                // const value = e.target.value.replace(/^\D*(\d*(?:\.\d{0, foundUdtInfo.decimals})?).*$/g, '$1');
+                const value = e.target.value.replace(/^\D*(\d*(?:\.\d*)?).*$/g, '$1');
+                void formik.setFieldValue('amount', fixString(value, foundUdtInfo.decimals), true);
+              }}
+              value={formik.values.amount}
+              onBlur={formik.handleBlur}
+            />
+            <Typography.Text type="danger">{formik.errors.amount}</Typography.Text>
+          </Form.Item>
 
-        <Form.Item>
-          <Button loading={isLoading} onClick={() => formik.submitForm()} type="primary">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button
+              disabled={!formik.isValid || !formik.dirty}
+              loading={isLoading}
+              onClick={() => formik.submitForm()}
+              type="primary"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
     </StyleWrapper>
   );
 };
