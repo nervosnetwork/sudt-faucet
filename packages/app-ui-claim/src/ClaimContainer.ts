9c2809c1832e8383487a0fcb132530372b6ed899
homura <homura.dev@gmail.com>
2021-09-10 11:03:13 +0800

    fix: cannot enter the page for the first time

--- packages/app-ui-claim/src/ClaimContainer.ts
+++ packages/app-ui-claim/src/ClaimContainer.ts
@@ -1,51 +1,18 @@
-import { CkitInitOptions, CkitProvider, predefined, UnipassWallet } from '@ckitjs/ckit';
-import { useLocalStorage } from '@rehooks/local-storage';
+import { CkitProvider, UnipassWallet } from '@ckitjs/ckit';
 import { RpcClient } from '@sudt-faucet/commons';
 import { useEffect, useMemo, useState } from 'react';
 import { createContainer } from 'unstated-next';
-
-const CLAIM_SECRET_PARAM_KEY = 'claim_secret';
-
-function getConfig(): { ckitConfig: CkitInitOptions; mercuryUrl: string; ckbRpcUrl: string; unipassUrl: string } {
-  const network = process.env.NETWORK;
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const ckitConfig = network === 'Lina' ? (predefined.Lina as CkitInitOptions) : predefined.Aggron;
-
-  return {
-    ckitConfig,
-    ckbRpcUrl: process.env.MERCURY_URL ?? 'https://testnet.ckb.dev/rpc',
-    mercuryUrl: process.env.RPC_URL ?? 'https://testnet.ckb.dev/indexer',
-    unipassUrl: process.env.UNIPASS_URL ?? 'https://unipass.xyz',
-  };
-}
+import { useClaimSecret } from './hooks/useClaimSecret';
+import { useGlobalConfig } from './hooks/useGlobalConfig';
 
 export const ClaimContainer = createContainer(() => {
-  const [config] = useLocalStorage('config', getConfig());
-  const [claimSecret, setClaimSecret, clearClaimSecret] = useLocalStorage('claim_secret');
+  const config = useGlobalConfig();
+  const [claimSecret, clearClaimSecret] = useClaimSecret();
   const [provider, setProvider] = useState<CkitProvider>();
   const [wallet, setWallet] = useState<UnipassWallet>();
   const [address, setAddress] = useState<string>();
 
   const client = useMemo(() => new RpcClient(), []);
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search.slice(1));
-
-    const secret = searchParams.get(CLAIM_SECRET_PARAM_KEY);
-    if (!secret) return;
-
-    searchParams.delete(CLAIM_SECRET_PARAM_KEY);
-
-    const { pathname, host, protocol } = window.location;
-
-    const qs = searchParams.toString();
-    const newUrl = `${protocol}//${host}${pathname}${qs ? `?${qs}` : ''}`;
-
-    window.history.replaceState({ path: newUrl }, '', newUrl);
-    setClaimSecret(secret);
-  }, [setClaimSecret]);
 
   useEffect(() => {
     void (async () => {
@@ -56,17 +23,13 @@ export const ClaimContainer = createContainer(() => {
 
       wallet.on('signerChanged', (signer) => Promise.resolve(signer.getAddress()).then(setAddress));
 
+      const adapter = new UnipassWallet.UnipassRedirectAdapter({ host: config.unipassUrl });
+      if (adapter.getLoginDataFromCache() || adapter.hasLoginInfo()) wallet.connect();
+
       setProvider(provider);
       setWallet(wallet);
     })();
-  }, [config.ckbRpcUrl, config.ckitConfig, config.mercuryUrl]);
-
-  useEffect(() => {
-    if (!wallet) return;
-
-    const adapter = new UnipassWallet.UnipassRedirectAdapter({ host: config.unipassUrl });
-    if (adapter.getLoginDataFromCache() || adapter.hasLoginInfo()) wallet.connect();
-  }, [wallet, config.unipassUrl]);
+  }, [config.ckitConfig, config.ckbRpcUrl, config.mercuryUrl, config.unipassUrl]);
 
   return { address, wallet, provider, client, claimSecret, clearClaimSecret };
 });
--- packages/app-ui-claim/src/pages/claim.tsx
+++ packages/app-ui-claim/src/pages/claim.tsx
@@ -1,7 +1,10 @@
-import { Button, Modal, Typography } from 'antd';
+import { Button, Modal, Result } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
 import { ClaimContainer } from '../ClaimContainer';
+import { useClaimSecret } from '../hooks/useClaimSecret';
+import { useGlobalConfig } from '../hooks/useGlobalConfig';
+import { Address } from './address';
 
 const Wrapper = styled.div`
   height: calc(90% - 40px);
@@ -21,14 +24,22 @@ const Wrapper = styled.div`
   }
 `;
 const Claim: React.FC<{ address: string; claimSecret: string }> = ({ address, claimSecret }) => {
+  const config = useGlobalConfig();
   const { client } = ClaimContainer.useContainer();
+  const [, clearClaimSecret] = useClaimSecret();
 
   async function claim() {
     return client.claim_sudt({ claimSecret, address }).then(
       () => {
+        clearClaimSecret();
+
         Modal.success({
           title: 'Congratulation',
           content: <p>Successfully claimed, go to my wallet to see</p>,
+          okText: 'To Wallet',
+          onOk: () => {
+            window.location.href = config.walletUrl;
+          },
         });
       },
       (e: Error) => {
@@ -42,12 +53,26 @@ const Claim: React.FC<{ address: string; claimSecret: string }> = ({ address, cl
 
   return (
     <Wrapper>
-      <Typography className="content-text">
-        A claim invitation can only be claimed once, are you sure you want to claim to this address?
-        <br />
-        <Typography.Text copyable>{address}</Typography.Text>
-      </Typography>
-      <Button onClick={claim}>Claim</Button>
+      <Result
+        title={
+          <div>
+            Claim to <Address address={address} />
+          </div>
+        }
+        status="info"
+        subTitle={
+          <div>
+            A claim invitation can only be claimed once, are you sure you want to claim to this address?
+            <br />
+            {address}
+          </div>
+        }
+        extra={
+          <Button size="large" type="primary" onClick={claim}>
+            Claim
+          </Button>
+        }
+      />
     </Wrapper>
   );
 };
