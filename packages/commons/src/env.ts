9415c4ef228dd5a49c2667be88629046b0149db8
Yonghui Lin <homura.dev@gmail.com>
2021-09-22 16:35:37 +0800

    feat: check env before app launch (#61)
    * feat: check env before app launch
    
    * feat: avoid using the recovered lock
    
    * chore: env for development
    
    * refactor: eslint
    
    * chore: issuer html title
    
    * chore: rm default db password

--- packages/commons/src/env.ts
+++ packages/commons/src/env.ts
@@ -1,12 +1,17 @@
-export function read<T = string>(key: string, defaults: T, parse: (x: string) => T): T {
+export function read<T = string>(key: string, parse: (x: string) => T, defaults?: T): T {
   const val = process.env[key];
-  return val ? parse(val) : defaults;
+
+  if (val !== undefined) return parse(val);
+
+  if (defaults !== undefined) return defaults;
+
+  throw new Error(`Cannot find ${key} in environment variables`);
 }
 
-export function readAsNum(key: string, defaults: number): number {
-  return read(key, defaults, Number);
+export function readAsNum(key: string, defaults?: number): number {
+  return read(key, Number, defaults);
 }
 
-export function readAsStr(key: string, defaults: string): string {
-  return read(key, defaults, String);
+export function readAsStr(key: string, defaults?: string): string {
+  return read(key, String, defaults);
 }
