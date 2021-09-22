972d36360382944b79966f2e0da9eaf959997bc3
mkxbl <vyclzn@gmail.com>
2021-09-22 20:04:14 +0800

    chore: split docker-compose

--- packages/app-server-issuer/src/db/knexfile.ts
+++ packages/app-server-issuer/src/db/knexfile.ts
@@ -1,6 +1,7 @@
 const password = process.env.MYSQL_PASSWORD || '123456';
 const host = process.env.MYSQL_HOST || '127.0.0.1';
 const port = process.env.MYSQL_PORT || '3306';
+
 const knexConfig = {
   development: {
     client: 'mysql2',
@@ -18,7 +19,19 @@ const knexConfig = {
 
   staging: {},
 
-  production: {},
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: host,
+      database: 'sudt_faucet',
+      port: port,
+      user: 'root',
+      password: password,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
 };
 
 export default knexConfig;
